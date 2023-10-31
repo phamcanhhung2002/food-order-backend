7908c31473d83535b405ee720159b92417a067f8
Hung Pham <hung.pham@hcmut.edu.vn>
2023-10-31 16:07:15 +0700

    Add constants

--- src/signup/signup.service.js
+++ src/signup/signup.service.js
@@ -3,6 +3,7 @@ import {
   CONFLICT_USER_MESSAGE,
   SALT_ROUNDS,
   USER_CREATED_MESSAGE,
+  HTTP,
 } from "../../constants/index.js";
 import bcrypt from "bcrypt";
 import { db } from "../utils/db.server.js";
@@ -10,7 +11,9 @@ import { db } from "../utils/db.server.js";
 export const register = async (req, res) => {
   const { username, password } = req.body;
   if (!username || !password)
-    return res.status(400).json({ message: REQUIRED_INFO_MESSAGE });
+    return res
+      .status(HTTP.BAD_REQUEST)
+      .json({ message: REQUIRED_INFO_MESSAGE });
   // check for duplicate usernames in the db
   const duplicate = await db.customer.findUnique({
     where: {
@@ -19,18 +22,18 @@ export const register = async (req, res) => {
   });
 
   if (duplicate)
-    return res.status(409).json({ message: CONFLICT_USER_MESSAGE }); //Conflict
+    return res.status(HTTP.CONFLICT).json({ message: CONFLICT_USER_MESSAGE }); //Conflict
 
   try {
     //encrypt the password
     const hashedPassword = await bcrypt.hash(password, SALT_ROUNDS);
     //store the new username
-    const newUser = { username: username, hashKey: hashedPassword };
+    const newUser = { username: username, hashPassword: hashedPassword };
 
     await db.customer.create({ data: newUser });
 
-    res.status(201).json({ success: USER_CREATED_MESSAGE(username) });
+    res.status(HTTP.CREATED).json({ success: USER_CREATED_MESSAGE(username) });
   } catch (err) {
-    res.status(500).json({ message: err.message });
+    res.status(HTTP.INTERNAL_SERVER_ERROR).json({ message: err.message });
   }
 };
