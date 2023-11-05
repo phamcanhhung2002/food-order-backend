0580c3925e608d34e28324cc14db92cfc75fa641
Hung Pham <hung.pham@hcmut.edu.vn>
2023-11-05 17:29:29 +0700

    Add filter, sort and validate params

--- src/apis/food/food.service.js
+++ src/apis/food/food.service.js
@@ -3,22 +3,76 @@ import {
   DEFAULT_PAGE,
   FOODS_PER_PAGE,
   HTTP,
+  SORT_OPTIONS,
+  ASC,
+  DEFAULT_SORT_OPTION,
 } from "../../constants/index.js";
 import { db } from "../../utils/db.server.js";
 
+const validateParams = (req) => {
+  let { page, sort, ord, cat, price } = req.query;
+
+  // If not send, use default options
+  page = parseInt(page) || DEFAULT_PAGE;
+  ord = ord || DESC;
+  sort = sort || DEFAULT_SORT_OPTION;
+  // If not send, mean don't filter using these params
+  cat = parseInt(cat);
+  let maxPrice, minPrice;
+  if (price) {
+    [minPrice, maxPrice] = price.split("-").map((p) => parseFloat(p));
+  }
+
+  // If specify page, it must be greater than zero
+  // If specify sort, it must be in sort options
+  // If specify ord, it must be in order options
+  // if specify price, both min and max must be not nullish
+  const isBadParams =
+    page < 0 ||
+    !SORT_OPTIONS.includes(sort) ||
+    ![DESC, ASC].includes(ord) ||
+    (maxPrice && !minPrice) ||
+    (!maxPrice && minPrice);
+
+  return { page, ord, sort, cat, minPrice, maxPrice, isBadParams };
+};
+
 export const getAllFoods = async (req, res) => {
   try {
-    const page = parseInt(req.query.page) || DEFAULT_PAGE;
+    const { page, ord, sort, cat, minPrice, maxPrice, isBadParams } =
+      validateParams(req);
+    if (isBadParams) return res.sendStatus(HTTP.BAD_REQUEST);
+
     // Caculate skip and take param
     const skip = (page - 1) * FOODS_PER_PAGE;
     const take = FOODS_PER_PAGE;
-    const numItemsPromise = db.food.count();
+
+    // Create orderBy
+    const orderBy = {};
+    orderBy[sort] = ord;
+
+    // Create where
+    const where = {};
+    if (cat) where.categoryId = cat;
+    if (minPrice) where.currentPrice = { gte: minPrice, lte: maxPrice };
+
+    const numItemsPromise = db.food.count({ where });
     const foodsPromise = db.food.findMany({
       skip,
       take,
-      orderBy: {
-        createdDate: DESC,
+      select: {
+        id: true,
+        name: true,
+        price: true,
+        currentPrice: true,
+        images: {
+          select: {
+            imageId: true,
+          },
+        },
       },
+      where,
+      orderBy,
     });
 
     const [foods, numItems] = await Promise.all([
--- src/constants/prisma.js
+++ src/constants/prisma.js
@@ -1,1 +1,4 @@
 export const DESC = "desc";
+export const ASC = "asc";
+export const SORT_OPTIONS = ["currentPrice", "energy", "rating", "createdDate"];
+export const DEFAULT_SORT_OPTION = "createdDate";
