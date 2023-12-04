093199e6ab02bde10bc1d40d53e21525d3e59abd
Anoencs <57495997+Anoencs@users.noreply.github.com>
2023-12-05 00:35:33 +0700

    Update auth.js

--- src/constants/auth.js
+++ src/constants/auth.js
@@ -3,7 +3,7 @@ export const CONFLICT_USER_MESSAGE = "This username is already existed.";
 export const USER_CREATED_MESSAGE = (username) =>
   `New username ${username} created!`;
 export const SALT_ROUNDS = 10;
-export const ACCESS_TK_EXP_TIME = "1d";
+export const ACCESS_TK_EXP_TIME = "30s";
 export const RERESH_TK_EXP_TIME = "1d";
 export const USER_TABLE_NAME = (userRoles) =>
   userRoles == 1 ? "admin" : "customer";
