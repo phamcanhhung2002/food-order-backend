ecdc44b3b1c54d86807b41dbcd1995cc1093709b
johnnyungirl <johnnyungirl@gmail.com>
2023-12-20 20:33:13 +0700

    fix discountService

--- src/apis/checkout/checkout.service.js
+++ src/apis/checkout/checkout.service.js
@@ -102,3 +102,42 @@ export const checkoutReview=async({orderId,customerId,discountCodeId,address})=>
         checkout_order
     }
 }
+export const getDiscountTotal=async({orderId,customerId,discountCodeId})=>{
+    const foundOrder=await db.Order.findFirst({where:{id:orderId}})
+    if(!foundOrder) throw new Error(`Order no found`)
+    const foundDiscocunt=await db.Voucher.findFirst({where:{code:discountCodeId}})
+    if(!foundDiscocunt) throw new Error(`message:Discount no found:${HTTP.BAD_REQUEST}`)
+    const checkout_order={totalPrice:0,feeship:0,totalDiscount:0,totalCheckOut:0,tax:0}
+    const foods_new=[]
+    const foods=await db.FoodsOnOrders.findMany({where:{orderId:orderId}})
+    const checkProductServer=await checkProductByServer(foods)
+    if(!checkProductServer) throw new Error('food in order invalid, check again',HTTP.BAD_REQUEST)
+    const checkOutPrice= await checkProductServer.reduce((acc,food)=>{
+            return acc+(food.quantity*food.price)
+        },0)
+    const ItemCheckOut={
+        discountCode:discountCodeId,
+        priceRaw:checkOutPrice,
+        priceApplyDiscount:checkOutPrice,
+        tax:0,
+        item_products:checkProductServer
+    }
+    if(discountCodeId!==null){
+        const {amount,tax}=await discountService.getDiscountAmount({foods:checkProductServer,codeId:discountCodeId})
+        checkout_order.totalDiscount+=amount
+        if(amount>0){
+            ItemCheckOut.priceApplyDiscount=checkOutPrice-amount-tax
+        }
+        ItemCheckOut.tax=tax
+    }
+    checkout_order.totalCheckOut+=ItemCheckOut.priceApplyDiscount
+    checkout_order.totalPrice+=ItemCheckOut.priceRaw
+    checkout_order.tax=ItemCheckOut.tax
+    foods_new.push(ItemCheckOut)
+    await db.Order.update({where:{id:orderId},data:{status:STATUS.COMPLETED,voucherId:foundDiscocunt.id,tax:checkout_order.tax}})
+    return {
+        foods:foods,
+        foods_new:foods_new,
+        checkout_order
+    }
+}
